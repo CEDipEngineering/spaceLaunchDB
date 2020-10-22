bd3493e58a18b9058d383390065b5ae8ac037dec
Carlos Dip <cadudip@hotmail.com>
2020-10-22 10:01:29 -0300

    Changed supporting rocket API version to 2.0.0.

--- db.js
+++ db.js
@@ -4,23 +4,19 @@ mongoose.connect("mongodb://localhost:27017/spacLaunchDB");
 
 var launchSchema = new mongoose.Schema({
     name : String, 			//launches[0]
-    id : Number,			//launches[0]
+    id : String,			//launches[0]
     launchtime : String, 		//launches[0]
-    status : Number, 			//launches[0]
+    status : String, 			//launches[0]
     tbddate : Number, 			//launches[0] 
     holdreason: String,  		//launches[0]
     failreason: String, 		//launches[0]
     mapurl : String, 		//launches[0] -> location -> pads[0]
     padname : String,  		//launches[0] -> location -> pads[0]
     agenciename : String, 			//launches[0] -> location -> pads[0] -> agencies[0]
-    abbrev : String, 		//launches[0] -> location -> pads[0] -> agencies[0]
-    agenciewikiurl : String, 	//launches[0] -> location -> pads[0] -> agencies[0]
-    imageurl : [String],  		//launches[0] -> rocket 
+    imageurl : String,  		//launches[0] -> rocket 
     missioname : String,  		//launches[0] -> missions[0] -> name
     missiondescription : String,     //launches[0] -> missions[0] -> name
-    missionwikiurl: String, 		//launches[0] -> missions[0] -> name
     missiontype : String, 		//launches[0] -> missions[0] -> name
-    vidurls : [String] 		//launches[0]
 }, {collection: "launches"}
 );
 
--- routes/index.js
+++ routes/index.js
@@ -9,74 +9,94 @@ router.get('/', function (req, res, next) {
 
 /*  GET launch and fill db */
 router.get("/updateDB", function (req, res, next) {
-  fetch("https://launchlibrary.net/1.4/launch/1000").then(response => response.json()).then(data => {
-    console.log(data);
-    var db = require("../db")
-
-    const rootDir = data.launches[0];
-    const locationDir = rootDir.location.pads[0];
-    const missionsDir = rootDir.missions[0];
-    const name = rootDir.name;
-    const id = rootDir.id;
-    const launchTime = rootDir.windowStart;
-    const status = rootDir.status;
-    const tbddate = rootDir.tbddate;
-    const holdreason = rootDir.holdreason;
-    const failreason = rootDir.failreason;
-    const mapurl = locationDir.mapURL;
-    const padname = locationDir.name;
-    const agencyName = missionsDir.agencies[0].name;
-    const abbrev = missionsDir.agencies[0].abbrev;
-    const agencyWikiURL = missionsDir.agencies[0].wikiURL;
-    const rocketImage = rootDir.rocket.imageURLs;
-    const missionName = missionsDir.name;
-    const missionDescription = missionsDir.description;
-    const missionWikiURL = missionsDir.wikiURL;
-    const missionType = missionsDir.typeName;
-    const vidURLs = rootDir.vidURLs;
-
-
-    var Launches = db.Mongoose.model('launches', db.LaunchSchema, 'launches'); 
-    var launch = new Launches({ 
-      name : name, 			//launches[0]
-      id : id,			//launches[0]
-      launchtime : launchTime, 		//launches[0]
-      status : status, 			//launches[0]
-      tbddate : tbddate, 			//launches[0] 
-      holdreason: holdreason,  		//launches[0]
-      failreason: failreason, 		//launches[0]
-      mapurl : mapurl, 		//launches[0] -> location -> pads[0]
-      padname : padname,  		//launches[0] -> location -> pads[0]
-      agenciename : agencyName, 			//launches[0] -> location -> pads[0] -> agencies[0]
-      abbrev : abbrev, 		//launches[0] -> location -> pads[0] -> agencies[0]
-      agenciewikiurl : agencyWikiURL, 	//launches[0] -> location -> pads[0] -> agencies[0]
-      imageurl : rocketImage,  		//launches[0] -> rocket 
-      missioname : missionName,  		//launches[0] -> missions[0] -> name
-      missiondescription : missionDescription,     //launches[0] -> missions[0] -> name
-      missionwikiurl: missionWikiURL, 		//launches[0] -> missions[0] -> name
-      missiontype : missionType, 		//launches[0] -> missions[0] -> name
-      vidurls : vidURLs 		//launches[0]
-    }); 
-
-    launch.save(function (err) { if (err) { 
-      console.log("Error! " + err.message); returnerr; 
-    } else { 
-      console.log("Post saved"); 
-    } 
+  var db = require("../db")
+  for (let i = 0; i<19; i++){
+    fetch("https://ll.thespacedevs.com/2.0.0/launch/?limit=100&offset=" + 100*i).then(response => response.json()).then(data => {
+      for (let i = 0; i < data.results.length; i++) {
+        const rootDir = data.results[i];
+        next = data.next;
+        const name = rootDir.name;
+        const id = rootDir.id;
+        const launchTime = rootDir.window_start;
+        const status = rootDir.status.name;
+        const tbddate = rootDir.tbddate;
+        const holdreason = rootDir.holdreason;
+        const failreason = rootDir.failreason;
+        const mapurl = rootDir.pad.map_url;
+        const padname = rootDir.pad.name;
+        const agencyName = rootDir.launch_service_provider.name;
+        const rocketImage = rootDir.image;
+  
+        if (rootDir.mission){
+          var missionName = rootDir.mission.name;
+          var missionDescription = rootDir.mission.description;
+          var missionType = rootDir.mission.typeName;
+        } else {
+          var missionName = null;
+          var missionDescription = null;
+          var missionType = null;
+        }
+  
+        var Launches = db.Mongoose.model('launches', db.LaunchSchema, 'launches');
+        var launch = new Launches({
+          name: name, 			//launches[0]
+          id: id,			//launches[0]
+          launchtime: launchTime, 		//launches[0]
+          status: status, 			//launches[0]
+          tbddate: tbddate, 			//launches[0] 
+          holdreason: holdreason,  		//launches[0]
+          failreason: failreason, 		//launches[0]
+          mapurl: mapurl, 		//launches[0] -> location -> pads[0]
+          padname: padname,  		//launches[0] -> location -> pads[0]
+          agenciename: agencyName, 			//launches[0] -> location -> pads[0] -> agencies[0]
+          imageurl: rocketImage,  		//launches[0] -> rocket 
+          missioname: missionName,  		//launches[0] -> missions[0] -> name
+          missiondescription: missionDescription,     //launches[0] -> missions[0] -> name
+          missiontype: missionType, 		//launches[0] -> missions[0] -> name
+        });
+  
+        launch.save(function (err) {
+          if (err) {
+            console.log("Error! " + err.message); 
+            return err;
+          } else {
+            console.log("Post saved");
+          }
+        });
+        // res.end(JSON.stringify(data));
+      }
     });
-
-
-    
-    res.end(JSON.stringify(data));
-    return;
-  })
+  }
 });
 
 router.get("/visualizeDB", function (req, res, next) {
   var db = require("../db");
-  var Launches = db.Mongoose.model('launches', db.LaunchSchema, 'launches'); 
-  Launches.find({}).lean().exec(function(e,docs){
+  var Launches = db.Mongoose.model('launches', db.LaunchSchema, 'launches');
+  Launches.find({}).lean().exec(function (e, docs) {
+    console.log(docs.length)
     res.end(JSON.stringify(docs));
+
+  });
+});
+
+router.get("/testNewAPI", function (req, res, next) {
+  fetch("https://ll.thespacedevs.com/2.0.0/launch/?limit=10").then(response => response.json()).then(data => {
+      console.log(data);
+      res.end(JSON.stringify(data.results[3], null, 2));
+});
+})
+
+router.get("/clearDB", function (req, res, next) {
+  var db = require("../db");
+  var Launches = db.Mongoose.model('launches', db.LaunchSchema, 'launches');
+  Launches.find({}).remove(function (err) {
+    if (err) {
+      res.status(500).json({error:err.message});
+      res.end();
+      return;
+    }
+    res.json({success:true});
+    res.end();
   });
 });
 
