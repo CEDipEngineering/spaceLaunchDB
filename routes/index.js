a501973a9821a602334bb128c18d7709a3ae4727
Carlos Dip <cadudip@hotmail.com>
2020-10-25 16:01:51 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -108,7 +108,8 @@ router.get("/byAgency", function (req, res, next) {
   for (var i of selAgencies.split(',')) {
     console.log("Oba: " + i)
   }
-  var queryResult = db.launches.find({'agencieName': selAgencies.split(',')[0]}).pretty()
+  var Launches = db.Mongoose.model('launches', db.LaunchSchema, 'launches');
+  var queryResult = Launches.find({'agencieName': selAgencies.split(',')[0]}).pretty()
   res.render("test", {'test': queryResult})
 });
 
