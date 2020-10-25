25f50b1abe4bc8f1e27cb29c957fd4f24824b1b2
Carlos Dip <cadudip@hotmail.com>
2020-10-25 14:59:13 -0300

    Implemented connection to MongoDB Atlas

--- db.js
+++ db.js
@@ -1,6 +1,19 @@
 const { Int32 } = require("mongodb");
 var mongoose = require("mongoose");
-mongoose.connect("process.env.MONGODB_URI || mongodb://localhost:27017/spacLaunchDB");
+const db = process.env.MONGODB_URL;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
 
 var launchSchema = new mongoose.Schema({
     name : String, 			//launches[0]
